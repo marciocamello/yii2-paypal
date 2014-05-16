86892ccf1c1c540868c34484e26296a9c9a50409
Andrey Klimenko <andrey.iemail@gmail.com>
2014-05-16 15:37:05 +0300

    yii2-paypal-1 Create basic architecture

--- Paypal.php
+++ Paypal.php
@@ -7,13 +7,25 @@
 
 namespace ak\paypal;
 
+use yii\base\Component;
+
 /**
  * Class Paypal.
  *
  * @package ak
  * @author Andrey Klimenko <andrey.iemail@gmail.com>
  */
-class Paypal
+class Paypal extends Component
 {
+    //region API settings
+    public $user;
+    public $password;
+    public $signature;
+    public $isProduction = false;
+    public $currency = 'USD';
 
+    private $version = '3.0';
+    //endregion
+
+    protected $errors = [];
 } 
